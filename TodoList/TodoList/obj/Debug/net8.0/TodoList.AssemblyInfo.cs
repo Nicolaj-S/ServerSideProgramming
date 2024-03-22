47638375648f88c8f213f9c7a0353d0ea57bb734
nicolaj1449 <48961648+Nicolaj-S@users.noreply.github.com>
2024-03-22 10:21:19 +0100

    minor change

--- TodoList/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb930c675a709e0cd7a3e79d2a6cae6691336062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8103fc2c673af69cf79d63160e98a6d09c6a6c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
